01ce391044179deb501df369f07a5f2f3537063c
Giovanna Lino <106554500+gigigabytes@users.noreply.github.com>
2023-04-10 18:53:48 +0000

    tentativa da questao

---
+++ beecrowd/2377.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Program {
+  public static void Main(string[] args{
+  int l, d, k, p;
+
+  int l, d = int.Parse(Console.ReadLine());
+  int k, p = int.Parse(Console.ReadLine());
+
+pedagio = ((int)l/d);
+
+gasolina = ((int)l*k);
+pedagios = (int)pedagio*p);
+
+total = ((int) gasolina + pedagios);
+
+Console.WriteLine(total);
+  
+  }
+  
+}
