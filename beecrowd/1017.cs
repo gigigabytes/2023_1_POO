f5405cd59da2db7f96c4f583b446b458ebf68604
gigigabytes <106554500+gigigabytes@users.noreply.github.com>
2023-04-19 17:29:49 +0000

    change

--- beecrowd/1017.cs
+++ beecrowd/1017.cs
@@ -1,3 +1,16 @@
 using System; 
 
-/*resolver questão ainda*/
+class Program {
+  public static void Main () {
+    
+  string[] x = (Console.ReadLine().Split());
+  int tempo = int.Parse(x[0]);
+  int velocidade = int.Parse(x[1]);
+
+int litros = (tempo*velocidade)/12;
+
+Console.WriteLine(litros);
+
+    }
+
+}
