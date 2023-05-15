cb62d4fe8cffe48537fc600198beaf019d9fad51
gigigabytes <106554500+gigigabytes@users.noreply.github.com>
2023-05-15 18:56:25 +0000

    exercicio lista07

---
+++ POO _Lista 07 _ Propriedades/retangulo.cs
@@ -0,0 +1,37 @@
+class Retangulo {
+  private double b, h, d;
+public double Base {
+  get { return b; }
+  set { if (value > 0) b = value; }
+
+}
+public double Altura {
+  get { return h;}
+  set { if (value > 0) h = value; }
+
+}
+  
+public double Area {
+  get { return b * h / 2; }
+
+}
+
+public double Diagonal {
+  get { return d = Math.Pow() }
+}
+// Falta colocar a formula  
+}
+
+class MainClass {
+  public static void Main () {
+  Triangulo x = new Triangulo();
+      x.Base = 10;
+      x.Altura = 20;
+    
+Console.WriteLine(x.Base);
+Console.WriteLine(x.Altura);
+Console.WriteLine(x.Area);
+Console.WriteLine(x.Diagonal);
+//add to string
+    }
+}
